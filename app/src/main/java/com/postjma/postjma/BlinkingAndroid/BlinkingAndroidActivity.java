24f4d687a9e2323b512b4c354d9c1350a6321890
post-j-ma <lymclymc123@gmail.com>
2017-04-23 17:38:20 -0400

    * Get rid of fullscreen handling. We don't need it and it's a little annoying. * Fix app name in build.gradle, so we can have BlinkingAndroid and petdemo both installed at the same time.

--- app/src/main/java/com/postjma/postjma/BlinkingAndroid/BlinkingAndroidActivity.java
+++ app/src/main/java/com/postjma/postjma/BlinkingAndroid/BlinkingAndroidActivity.java
@@ -23,87 +23,32 @@ import static android.R.drawable.sym_def_app_icon;
  */
 public class BlinkingAndroidActivity extends AppCompatActivity {
     /**
-     * Whether or not the system UI should be auto-hidden after
-     * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
-     */
-    private static final boolean AUTO_HIDE = true;
-
-    /**
-     * If {@link #AUTO_HIDE} is set, the number of milliseconds to wait after
-     * user interaction before hiding the system UI.
-     */
-    private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
-
-    /**
      * Some older devices needs a small delay between UI widget updates
      * and a change of the status and navigation bar.
      */
     private static final int UI_ANIMATION_DELAY = 300;
-    private final Handler mHideHandler = new Handler();
+    private final Handler mShowHideHandler = new Handler();
     private View mContentView;
-    private final Runnable mHidePart2Runnable = new Runnable() {
+    private final Runnable mShowHidePart2Runnable = new Runnable() {
         @SuppressLint("InlinedApi")
         @Override
         public void run() {
-            // Delayed removal of status and navigation bar
-
-            // Note that some of these constants are new as of API 16 (Jelly Bean)
-            // and API 19 (KitKat). It is safe to use them, as they are inlined
-            // at compile-time and do nothing on earlier devices.
-            mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
-                    | View.SYSTEM_UI_FLAG_FULLSCREEN
-                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
             toggleAndroid();
+            mShowHideHandler.postDelayed(mShowHidePart2Runnable, UI_ANIMATION_DELAY);
         }
     };
     private View mControlsView;
-    private final Runnable mShowPart2Runnable = new Runnable() {
-        @Override
-        public void run() {
-            // Delayed display of UI elements
-            ActionBar actionBar = getSupportActionBar();
-            if (actionBar != null) {
-                actionBar.show();
-            }
-            mControlsView.setVisibility(View.VISIBLE);
-            toggleAndroid();
-        }
-    };
 
     private void toggleAndroid()
     {
-        ImageView iv = (ImageView)
-                findViewById(R.id.imageView);
-        int x = sym_def_app_icon;
-        //int y = sym_contact_card;
-        Drawable xDraw = getResources().getDrawable(x);
-        //Drawable yDraw = getResources().getDrawable(y);
-        //Drawable yDraw = xDraw.mutate(); // didn't do anything?
-        Bitmap b = BitmapFactory.decodeResource(getResources(), sym_def_app_icon);
-        Paint p = new Paint();
-        Canvas c = new Canvas();
-        // hmm ... none of the canvas methods appear to have any effect on the imageView?
+        ImageView iv = (ImageView)findViewById(R.id.imageView);
+        Drawable xDraw = getResources().getDrawable(sym_def_app_icon);
         if (mCount > 0) {
-            p.setColor(Color.RED);
-            c.drawBitmap(b, 90.0f, 0.0f, p);
-            c.drawARGB(255, 255, 255, 0);
-            c.drawColor(Color.BLACK);
-            c.drawLine(0.0f, 0.0f, 200.0f, 200.0f, p);
-            iv.draw(c);
             if (mOldDraw == null) mOldDraw = iv.getDrawable();
-            //iv.setImageBitmap(b);
             iv.setImageDrawable(xDraw);
         }
         else
         {
-            //iv.setImageDrawable(yDraw);
-            p.setColor(Color.BLACK);
-            //b.eraseColor(Color.GREEN);
-            c.drawBitmap(b, 0.0f, 0.0f, p);
-            iv.draw(c);
             if (mOldDraw != null)
                 iv.setImageDrawable(mOldDraw);
             else
@@ -118,37 +63,12 @@ public class BlinkingAndroidActivity extends AppCompatActivity {
     private Drawable mOldDraw = null;
     private int mCount = 1;
 
-    private boolean mVisible;
-    private final Runnable mHideRunnable = new Runnable() {
-        @Override
-        public void run() {
-            hide();
-        }
-    };
-    /**
-     * Touch listener to use for in-layout UI controls to delay hiding the
-     * system UI. This is to prevent the jarring behavior of controls going away
-     * while interacting with activity UI.
-     */
-    private final View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
-        @Override
-        public boolean onTouch(View view, MotionEvent motionEvent) {
-            if (AUTO_HIDE) {
-                delayedHide(AUTO_HIDE_DELAY_MILLIS);
-            } //else {
-                toggleAndroid();
-            //}
-            return false;
-        }
-    };
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_fullscreen);
 
-        mVisible = true;
         mControlsView = findViewById(R.id.fullscreen_content_controls);
         mContentView = findViewById(R.id.fullscreen_content);
 
@@ -157,66 +77,8 @@ public class BlinkingAndroidActivity extends AppCompatActivity {
         mContentView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                toggle();
+                toggleAndroid();
             }
         });
-
-        // Upon interacting with UI controls, delay any scheduled hide()
-        // operations to prevent the jarring behavior of controls going away
-        // while interacting with the UI.
-        findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
-    }
-
-    @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-
-        // Trigger the initial hide() shortly after the activity has been
-        // created, to briefly hint to the user that UI controls
-        // are available.
-        delayedHide(100);
-    }
-
-    private void toggle() {
-        if (mVisible) {
-            hide();
-        } else {
-            show();
-        }
-    }
-
-    private void hide() {
-        // Hide UI first
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.hide();
-        }
-        mControlsView.setVisibility(View.GONE);
-        mVisible = false;
-
-        // Schedule a runnable to remove the status and navigation bar after a delay
-        mHideHandler.removeCallbacks(mShowPart2Runnable);
-        mHideHandler.postDelayed(mHidePart2Runnable, UI_ANIMATION_DELAY);
-    }
-
-    @SuppressLint("InlinedApi")
-    private void show() {
-        // Show the system bar
-        mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
-        mVisible = true;
-
-        // Schedule a runnable to display UI elements after a delay
-        mHideHandler.removeCallbacks(mHidePart2Runnable);
-        mHideHandler.postDelayed(mShowPart2Runnable, UI_ANIMATION_DELAY);
-    }
-
-    /**
-     * Schedules a call to hide() in [delay] milliseconds, canceling any
-     * previously scheduled calls.
-     */
-    private void delayedHide(int delayMillis) {
-        mHideHandler.removeCallbacks(mHideRunnable);
-        mHideHandler.postDelayed(mHideRunnable, delayMillis);
     }
 }
